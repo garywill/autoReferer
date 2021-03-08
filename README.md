07a83b73fac9799f78f044cf73708454b43286ca
garywill <garywill@disroot.org>
2021-03-08 14:00:05 +0800

    able to disable in some tabs or window

--- README.md
+++ README.md
@@ -1,11 +1,32 @@
-# autoreferer
+# Auto Referer
+
 Firefox addon. Control referer to protect privacy and not break web.
 
-HTTP header referer control policy:
-1. Any referer sent contain no more than `protocol://domain-name:port/`
-1. For web page's main frame:
-    1) If origin and target url have same domain, send referer
-    2) If origin and target url have different domain, no referer
-1. For web page resources, send referer
 
-I believe that can protect privacy enough and won't break web. If you find anything unexpected, welcome feedback.
+**Notice**: Due to browser bug on javascript `document.referrer` on Firefox 69+ ([1601496](https://bugzilla.mozilla.org/show_bug.cgi?id=1601496), [1601743](https://bugzilla.mozilla.org/show_bug.cgi?id=1601743)), using this addon we can get 70% of expected protection until Mozilla fixes that bug.
+
+Referer policy:
+
+1. For webs' top frame (i.e. clicking link, navigating, redirecting etc.):
+   
+   1. If origin and target url have same domain, allow trimmed referer
+   2. If origin and target url have different domain, no referer
+
+2. For in-page resources (images, videos, js, css etc.), allow trimmed referer (this is **the key to not break** most webs, also a balance between privacy and experience)
+
+3. Trim referer: Any referer should be no more than `http(s)://domain-name:port/` (like Firefox's native `about:config` setting `network.http.referer.trimmingPolicy = 2`).
+
+4. Not allow referer that not starts with "http" or "https". (Please feedback if you find something broken due to this)
+
+5. No referer when downgrade from HTTPS to HTTP
+
+I believe that can protect privacy enough and won't break web.
+
+If user find a web broken, user can temporary set this addon disabled via toolbar button for:
+
+- this one tab
+- this one tab and new tabs opened by this tab
+- this one window
+- globally
+
+there's toolbar button badge indicating disabling status.
--- background.js
+++ background.js
@@ -1,91 +1,139 @@
-chrome.webRequest.onBeforeSendHeaders.addListener(
-    rewriteRefererHeader,
+const addon_name = "Auto Referer";
+const default_title = addon_name;
+
+var global_enabled = false; 
+
+var list_w_disable = []; // window off list
+var list_h_disable = []; // tab and sub tabs off list
+var list_t_disable = []; // tab off list
+
+setGlobalEnable();
+
+browser.tabs.onRemoved.addListener( (tabid, removeInfo) => {
+    const wid = removeInfo.windowId;
+    normalizeTab(tabid);
+});
+browser.windows.onRemoved.addListener((wid) => {
+    unsetWindowDisabled(wid);
+});
+browser.browserAction.onClicked.addListener((tab) => {
+    const tabid = tab.id;
+    const wid = tab.windowId;
+    
+    if ( ! isTabIn_list_h(tabid) && ! isTabIn_list_t(tabid) )
+    {
+        setTab_t(tabid);
+    }else if ( isTabIn_list_t(tabid) ) 
+    {
+        setTab_h(tabid);
+    }else if ( isTabIn_list_h(tabid) )
+    {
+        normalizeTab(tabid);
+    }
+    
+});
+browser.tabs.onCreated.addListener( (tab) => {
+    if ( isTabIn_list_h(tab.openerTabId) ) {
+        setTab_h(tab.id);
+    }
+});
+//----------------------------------------------------------
+
+browser.webRequest.onBeforeSendHeaders.addListener(
+    onBeforeSendHeaders,
     {urls: ["<all_urls>"]},
     ["blocking", "requestHeaders"]
 ); 
 
-var enabled = true;
-
-function rewriteRefererHeader(e)
+async function onBeforeSendHeaders(details)
 {
-    if (!enabled) return;
-    const targetURL = e.url;
-    const resourceType = e.type;
-    const documentUrl = e.documentUrl;
-    const originUrl = e.originUrl;
-    
-    if (resourceType == "main_frame")
-    {
-        for (var i=0;i<e.requestHeaders.length;i++)
+    try{ 
+        const tabid = details.tabId;
+        if ( tabid < 0 ) return;
+        if ( ! global_enabled ) return;
+        if (isTabIn_list_h(tabid) || isTabIn_list_t(tabid) ) return;
+        const wid = (await browser.tabs.get(tabid)).windowId;
+        if( isWindowDisabled( wid ) ) return;
+        
+        const targetURL = details.url;
+        const resourceType = details.type;
+        //const documentUrl = details.documentUrl;
+        //const originUrl = details.originUrl;
+        
+        for (var i=0; i<details.requestHeaders.length; i++)
         {
-            if (e.requestHeaders[i].name.toLowerCase() === "referer")
+            const cur_header = details.requestHeaders[i];
+            if (cur_header.name.toLowerCase() === "referer" ||
+                cur_header.name.toLowerCase() === "referrer"
+            )
             {
-                if ( getUrlHost(e.requestHeaders[i].value) == getUrlHost(targetURL) )
-                {
-                    e.requestHeaders[i].value = removeUrlParts( e.requestHeaders[i].value );
-                }else{
-                    e.requestHeaders.splice(i,1);
-                    break;
-                }
+                var newReferer = null;
+                newReferer = getNewReferer(targetURL, cur_header.value, resourceType == "main_frame", resourceType == "sub_frame");
+                
+                cur_header.value = newReferer;
+                if (!newReferer)
+                    details.requestHeaders.splice(i,1);
             }
-        }
-    }else{
-        var obj_referer = null;
-        for(var header of e.requestHeaders) {
-            if(header.name.toLowerCase() === "referer" && header.value) {
-                obj_referer = header;
-                break;
-            }
-            
         }
         
-        if (obj_referer) obj_referer.value = removeUrlParts(obj_referer.value);
+        return {requestHeaders: details.requestHeaders};
+    } catch(err){ 
+        if ( ! err.message.startsWith("Invalid tab ID:") )   console.error(err);
     }
-    return {requestHeaders: e.requestHeaders};
 }
 
-function removeUrlParts(s)
+
+//===================================================
+// referer policy
+function getNewReferer(targetURL, oldReferer="", isTop, isSubframeTop){ 
+    var newReferer = "";
+    
+    if (isTop || isSubframeTop)
+        if ( getUrlHost(targetURL) === getUrlHost(oldReferer) )
+            newReferer = removeUrlParts(oldReferer);
+        else 
+            newReferer = "";
+    else
+        newReferer = removeUrlParts(oldReferer);
+
+
+    
+    // when httpS to http (downgrade)
+    if ( oldReferer.toLowerCase().startsWith("https://") && targetURL.toLowerCase().startsWith("http://") )
+        newReferer = "";
+    
+    // when url is other strange scheme that we don't know
+    if ( !getUrlHost(targetURL) || !getUrlHost(oldReferer) ) 
+        newReferer = "";
+    
+    // make sure referer only starts with http or https
+    if (!newReferer.toLowerCase().startsWith("https://") &&
+        !newReferer.toLowerCase().startsWith("http://")
+    )
+        newReferer = "";
+    
+    return newReferer;
+}
+function removeUrlParts(s) // 'http://example.com:8888/a/b/c' --> 'http://example.com:8888/'
 {
     try{
         var arr = s.split("/");
         var result = arr[0] + "//" + arr[2] + "/";
-        
         return result;
-    }catch(e){
+    }catch(err){
+        console.error(err);
         return s;
     }
 }
-
-function getUrlHost(s)
+function getUrlHost(s) // 'http://example.com:8888/a/b/c' --> 'example.com:8888'
 {
     try{
         var arr = s.split("/");
         var result = arr[2]
-        
         return result;
-    }catch(e){
+    }catch(err){
+        console.error(err);
         return s;
     }
 }
-
-browser.browserAction.onClicked.addListener(() => {
-    enabled = !enabled;
-    setBadgeStatus();
-    
-});
-
-function setBadgeStatus(s)
-{   
-    if (!enabled)
-    {
-        browser.browserAction.setBadgeText({ text: "Off" });
-        browser.browserAction.setBadgeBackgroundColor({ color: "#dd0000" });
-        browser.browserAction.setTitle({title: "Auto Referer (Disabled)"});
-    }
-    else{
-    
-        browser.browserAction.setBadgeText({ text: "" });
-        browser.browserAction.setBadgeBackgroundColor({ color: "#00BF00" });
-        browser.browserAction.setTitle({title: ""});
-    }
 }
