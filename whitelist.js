f0b2a0e2142db54bbae39f9ac014cfbe47d591f1
Martin Braun <martin.braun.1991@live.com>
2023-05-12 13:29:54 +0200

    Add itunesconnect.apple.com to Apple Developer Login whitelist

--- whitelist.js
+++ whitelist.js
@@ -1,7 +1,7 @@
 var whitelist = [
     "gitlab.com", 
     
-    [ "appstoreconnect.apple.com", "appleid.apple.com" ],
+    [ "appstoreconnect.apple.com", "appleid.apple.com", "itunesconnect.apple.com" ],
     
     [ "taobao.com", "alipay.com", "tmall.com",  "alibabagroup.com", "aliexpress.com", "alibaba.com", "1688.com",  "alimama.com" ], 
     
